38647bac13da72f49dbd27b6655a34b083ad1ad1
Damgeed <damgeed@users.noreply.github.com>
2026-05-23 12:35:32 +0800

    chore: remove temp verification script

--- _verify_import.py
+++
@@ -1,6 +0,0 @@
-"""Verify BillingService import."""
-from memory_bridge.services.billing_service import BillingService
-from memory_bridge.services import BillingService as BS
-
-print("OK - BillingService import successful")
-print(f"Class name: {BillingService.__name__}")
